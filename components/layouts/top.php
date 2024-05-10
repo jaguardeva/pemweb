8223953cd5da8bf26bc65b868417b47337415b89
Jaguar Deva <jaguardeva54@gmail.com>
2024-05-11 06:59:19 +0700

    modif: webkit scroll bar

--- components/layouts/top.php
+++ components/layouts/top.php
@@ -5,10 +5,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <style>
+    ::-webkit-scrollbar {
+      width: 8px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: grey;
+      cursor: pointer;
+    }
+  </style>
+
   <!-- flowbite -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
   <script src="https://cdn.tailwindcss.com"></script>
-
   <script>
     tailwind.config = {
       theme: {
