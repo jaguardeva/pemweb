76539dc31025731478a1339803e21f866fce18ca
Jaguar Deva <jaguardeva54@gmail.com>
2024-05-11 13:53:33 +0700

    fix: navbar bug

--- components/layouts/top.php
+++ components/layouts/top.php
@@ -17,20 +17,9 @@
   </style>
 
   <!-- flowbite -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="../../styles.css">
-  <!-- <script>
-    tailwind.config = {
-      theme: {
-        extend: {
-          backgroundImage: {
-            'foto-surabaya': "url('../../assets/images/sby.jpeg')",
-          }
-        }
-      }
-    }
-  </script> -->
-
+  <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" /> -->
+  <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+  <link rel="stylesheet" href="styles.css">
   <title>Document</title>
 </head>
 
