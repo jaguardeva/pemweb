c44355c87b38995e4cecb3768adad8e44a410174
Jaguar Deva <jaguardeva54@gmail.com>
2024-05-20 10:04:06 +0700

    feat: view report

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -26,23 +26,20 @@ $currentPage = basename($_SERVER['PHP_SELF']);
       <li>
         <a href="make-report.php"
           class="<?php echo $currentPage === "make-report.php" ? "bg-gray-100" : "bg-white"; ?> flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100">
-          <i class="fa-solid fa-file-pen text-lg text-gray-500"></i>
+          <i class="text-lg text-gray-500 fa-solid fa-file-pen"></i>
           <span class="flex-1 ms-3 whitespace-nowrap">Buat Laporan</span>
         </a>
       </li>
 
       <li>
-        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
-          <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 "
-            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-            <path
-              d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-          </svg>
-          <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
+        <a href="/reports.php"
+          class="<?php echo $currentPage === "reports.php" ? "bg-gray-100" : "bg-white"; ?> flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100">
+          <i class="text-xl text-gray-500 fa-solid fa-bullhorn"></i>
+          <span class="flex-1 ms-3 whitespace-nowrap">Laporan</span>
         </a>
       </li>
       <li>
-        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group">
+        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
           <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
             <path
--- make-report.php
+++ make-report.php
@@ -48,50 +48,50 @@ include "./layouts/dashboard/top.php";
 ?>
 
 <main class="p-4 my-14 sm:ml-64">
-  <h1 class="font-semibold text-3xl">Buat Laporan</h1>
-  <article class="flex flex-col items-center w-full mx-auto max-w-4xl border shadow-lg py-5 px-7">
-    <h1 class="text-2xl font-bold my-8 uppercase">Tulis Laporan Anda Di Sini</h1>
-    <form action="" class="w-full flex flex-col gap-4" method="POST" enctype="multipart/form-data">
-      <h1 class="font-semibold text-lg">Klasifikasi Laporan:</h1>
-      <div class="w-full flex gap-5 items-center my-2">
+  <h1 class="my-4 text-xl font-semibold md:text-3xl md:my-0">Buat Laporan</h1>
+  <article class="flex flex-col items-center w-full max-w-4xl py-5 mx-auto border shadow-lg px-7">
+    <h1 class="my-8 font-bold uppercase md:text-2xl">Tulis Laporan Anda Di Sini</h1>
+    <form action="" class="flex flex-col w-full gap-4" method="POST" enctype="multipart/form-data">
+      <h1 class="font-semibold md:text-lg">Klasifikasi Laporan:</h1>
+      <div class="flex items-center w-full gap-5 my-2">
         <div class="flex items-center gap-3">
           <label for="pengaduan" class="cursor-pointer">
-            <input type="radio" id="pengaduan" name="type" value="pengaduan" class="peer sr-only" required>
+            <input type="radio" id="pengaduan" name="type" value="pengaduan" class="sr-only peer" required>
             <div
-              class="flex items-center bg-gray-400 text-white gap-3  px-5 py-4  peer-checked:text-white peer-checked:bg-blue-500 rounded">
-              <h1 class="font-semibold text-lg">PENGADUAN</h1>
-              <i class="fa-solid fa-circle-check text-lg"></i>
+              class="flex items-center gap-3 px-5 py-4 text-white bg-gray-400 rounded peer-checked:text-white peer-checked:bg-blue-500">
+              <h1 class="text-sm font-semibold md:text-lg">PENGADUAN</h1>
+              <i class="text-sm md:text-lg fa-solid fa-circle-check"></i>
             </div>
           </label>
           <label for="aspirasi" class="cursor-pointer">
-            <input type="radio" id="aspirasi" name="type" value="aspirasi" class="peer sr-only" required>
+            <input type="radio" id="aspirasi" name="type" value="aspirasi" class="sr-only peer" required>
             <div
-              class="flex items-center bg-gray-400 text-white gap-3  px-5 py-4  peer-checked:text-white peer-checked:bg-blue-500 rounded">
-              <h1 class="font-semibold text-lg">ASPIRASI</h1>
-              <i class="fa-solid fa-circle-check text-lg"></i>
+              class="flex items-center gap-3 px-5 py-4 text-white bg-gray-400 rounded peer-checked:text-white peer-checked:bg-blue-500">
+              <h1 class="text-sm font-semibold md:text-lg">ASPIRASI</h1>
+              <i class="text-sm md:text-lg fa-solid fa-circle-check"></i>
             </div>
           </label>
         </div>
       </div>
       <div class="w-full space-y-3">
-        <label for="title" class="font-semibold text-lg">Judul</label>
-        <input class="w-full py-3 px-5 rounded outline-none border border-gray-300" value="<?= $_POST["title"] ?? "" ?>"
-          type="text" id="title" name="title" placeholder="Judul laporan..." required>
+        <label for="title" class="font-semibold md:text-lg">Judul</label>
+        <input class="w-full px-5 py-3 border border-gray-300 rounded outline-none" value="<?= $_POST["title"] ?? "" ?>"
+          type="text" id="title" name="title" placeholder="Judul laporan..." required autofocus>
       </div>
       <div class="w-full space-y-3">
-        <label for="body" class="font-semibold text-lg">Isi Laporan</label>
+        <label for="body" class="font-semibold md:text-lg">Isi Laporan</label>
         <textarea class=" w-full p-5 rounded outline-none border border-gray-300 h-[200px]" name="body" id="body"
           required><?= $_POST["body"] ?? "" ?></textarea>
       </div>
       <div class="flex flex-col gap-3">
-        <label for="image" class="font-semibold text-lg">Foto <span class="text-sm font-normal">(bukti sebagai penguat
+        <label for="image" class="text-lg font-semibold">Foto <span class="text-sm font-normal">(bukti sebagai penguat
             laporan
             laporan)</span></label>
         <input type="file" name="image" id="image">
-        <p class="text-sm text-red-500">*Format yang didukung: .jpg, .jpeg, .png</p>
+        <p class="text-xs text-red-500 md:text-sm">*Format yang didukung: .jpg, .jpeg, .png | ukuran maksimal: 2MB</p>
       </div>
       <button type="submit" name="submit"
-        class="bg-blue-500 hover:bg-blue-600 transition-all duration-100 text-white font-semibold px-5 py-3 rounded">Kirim
+        class="px-5 py-3 font-semibold text-white transition-all duration-100 bg-blue-500 rounded hover:bg-blue-600">Kirim
         Laporan</button>
     </form>
   </article>
